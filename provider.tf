075468f4d86b5b989d70c2cfa26e4d48216fcc38
Seokchan Ahn <sean9509@gmail.com>
2021-09-01 17:01:12 +0900

    Revert "Import terraform cloud (#5)"
    This reverts commit 184d17d3f3285d33e8ee3242483fa03c3b5c20ed.

--- provider.tf
+++ provider.tf
@@ -6,12 +6,3 @@ variable "github_token" {
 provider "github" {
   token = var.github_token
 }
-
-variable "terraform_cloud_token" {
-  type      = string
-  sensitive = true
-}
-
-provider "tfe" {
-  token = var.terraform_cloud_token
-}
