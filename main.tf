68ec4e9fd86b80964c69500d80c497681d82e042
sean-ahn <sean9509@gmail.com>
2021-09-01 15:48:26 +0900

    Add

--- main.tf
+++ main.tf
@@ -1,3 +1,27 @@
+resource "github_repository" "iac" {
+  name          = "iac"
+  visibility    = "public"
+  description   = "Infrastructure as Code"
+  has_wiki      = false
+  has_projects  = false
+  has_downloads = false
+  has_issues    = true
+  archived      = false
+  topics = [
+    "iac",
+    "terraform",
+    "terraform-cloud"
+  ]
+  allow_merge_commit     = false
+  allow_rebase_merge     = false
+  allow_squash_merge     = true
+  delete_branch_on_merge = true
+  auto_init              = null
+  gitignore_template     = null
+  license_template       = ""
+  homepage_url           = "https://app.terraform.io/app/sean-ahn/workspaces/iac"
+}
+
 resource "github_repository" "gotzdatacheck" {
   name          = "gotzdatacheck"
   visibility    = "public"
