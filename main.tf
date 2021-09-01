b2b63cb508584ec13514cee4b372e2d6d893b43e
sean-ahn <sean9509@gmail.com>
2021-09-01 15:42:24 +0900

    Use visibility

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "github_repository" "gotzdatacheck" {
   name          = "gotzdatacheck"
-  private       = false
+  visibility    = true
   description   = "gotzdatacheck checks that time/tzdata package has imported in main package"
   has_wiki      = false
   has_projects  = false
