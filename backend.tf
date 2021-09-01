f10ac044ad130da9837340e2df19c64b378a3d70
Seokchan Ahn <sean9509@gmail.com>
2021-09-01 17:01:40 +0900

    Revert "Import terraform cloud" (#6)
    This reverts commit 184d17d3f3285d33e8ee3242483fa03c3b5c20ed.

--- backend.tf
+++ backend.tf
@@ -2,10 +2,7 @@ terraform {
   required_providers {
     github = {
       source  = "integrations/github"
-      version = "~> 4.0"
-    }
-    tfe = {
-      version = "~> 0.25.0"
+      version = ">= 4.0"
     }
   }
 
