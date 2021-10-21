33fdb0bec0351581a630d3d0bb5d20649284d496
sean-ahn <sean9509@gmail.com>
2021-10-21 13:53:19 +0900

    Add user repository

--- github.tf
+++ github.tf
@@ -88,3 +88,34 @@ resource "github_repository" "blog-web" {
   license_template       = ""
   homepage_url           = "blog-web-flame.vercel.app"
 }
+
+resource "github_repository" "user" {
+  name          = "user"
+  visibility    = "public"
+  description   = ""
+  has_wiki      = false
+  has_projects  = false
+  has_downloads = false
+  has_issues    = true
+  archived      = false
+  topics = [
+    "go",
+    "golang",
+    "grpc",
+    "protocol-buffers",
+    "protobuf",
+    "buf-cli",
+    "buf-schema-registry",
+    "dynamodb",
+    "jwt"
+  ]
+  allow_merge_commit     = false
+  allow_rebase_merge     = false
+  allow_squash_merge     = true
+  delete_branch_on_merge = true
+  vulnerability_alerts   = true
+  auto_init              = null
+  gitignore_template     = null
+  license_template       = "mit"
+  homepage_url           = ""
+}
